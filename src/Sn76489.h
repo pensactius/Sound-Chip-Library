f071ad0317ab1717d8d3f1ece69b7ea35f4a1d45
PensActius <37923848+pensactius@users.noreply.github.com>
2021-09-30 20:14:28 +0200

    Update Sn76489.h

--- src/Sn76489.h
+++ src/Sn76489.h
@@ -1,3 +1,33 @@
+/*!
+ *  @file Sn76489.h
+ *
+ * Video Game Music Library
+ *
+ * Written by Andrés Mata (PensActius) 2021.
+ *
+ * This filel is part of the Sound Chip Library.
+ *
+ * MIT license, all text above must be included in any redistribution
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 #pragma once
 
 #include "ISoundChip.h"
